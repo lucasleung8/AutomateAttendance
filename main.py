9405e45f5625e5b84444301f1f6b9a85753b17ea
Lucas Leung <noemail>
2024-01-13 02:39:58 -0500

    asd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 ## AutoColumn Google Sheet
 ## By: Lucas Leung
 
-
 # setup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -9,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 # ---user configurable options---
-cellLink = "" # paste in your link to the cell from the attendance spreadsheet
+cellLink = "https://docs.google.com/spreadsheets/d/17mWdVgPJ0xo1qb0GAQLTVXSLu1_jl8bqeM3H4McVFeI/edit#gid=0&range=D1" # paste in your link to the cell from the attendance spreadsheet
 # driver = webdriver.Chrome() # assumes you use Chrome, uncomment whatever browser you use
 driver = webdriver.Edge()
 # driver = webdriver.Firefox()
@@ -18,11 +17,9 @@ driver = webdriver.Edge()
 driver.get(cellLink)
 
 # create new column to left, with via Actions API/Action Chains
-
 # other browsers: Alt + Shift + i, then c, then c
 actions = ActionChains(driver)
-
-actions.send_keys(Keys.RETURN)
+actions.send_keys(Keys.ALT + Keys.LEFT_SHIFT + "i" + "c" + "c")
 actions.perform()
 
 # delay
