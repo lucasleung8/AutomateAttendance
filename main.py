75de41d1d2749cf9106edf8b2eba74d889a7e5a0
Lucas Leung <noemail>
2024-01-13 03:49:21 -0500

    c

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-## AutoColumn Google Sheet tool
-## adds new column to 3 different spreadsheets
-## By: Lucas Leung
-
 # setup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -10,32 +6,49 @@ import time
 import datetime
 
 # ---user configurable options---
-cellLink = "https://docs.google.com/spreadsheets/d/17mWdVgPJ0xo1qb0GAQLTVXSLu1_jl8bqeM3H4McVFeI/edit#gid=0&range=D2" # paste in your link to the cell from the attendance spreadsheet
-# driver = webdriver.Chrome() # assumes you use Chrome, uncomment whatever browser you use
-driver = webdriver.Edge()
-# driver = webdriver.Firefox()
-
-# open link to cell
-driver.get(cellLink)
+ics4uLink = "https://docs.google.com/spreadsheets/d/17mWdVgPJ0xo1qb0GAQLTVXSLu1_jl8bqeM3H4McVFeI/edit#gid=0&range=D2" # paste in your link to the cell from the attendance spreadsheet
+tej34mLink = "https://docs.google.com/spreadsheets/d/1kPNp01St8eahSrfadigF5NHyBvvdxaplhzLufCYWfpU/edit#gid=0&range=D1" # paste in your link to the cell from the attendance spreadsheet
 
 # create datetime object from datetime class then format it nicely
 currentDate = datetime.datetime.now().strftime("%b %d")
 
-# create new column to left, with via Actions API/Action Chains
+# driver = webdriver.Chrome() # assumes you use Chrome, uncomment whatever browser you use
+driver = webdriver.Edge()
+# driver = webdriver.Firefox()
+
 actions = ActionChains(driver)
+
+# create new column to left, with via Actions API/Action Chains
+# stores actions then performs them
+# ics4u
+# open link to cell
+driver.get(ics4uLink)
 actions.key_down(Keys.LEFT_ALT).key_down(Keys.LEFT_SHIFT)
-actions.send_keys("i")
-actions.send_keys("c")
+actions.send_keys("i").send_keys("c")
 time.sleep(1)
 actions.send_keys("c")
 actions.key_up(Keys.LEFT_ALT).key_up(Keys.LEFT_SHIFT)
-actions.send_keys(currentDate)
+actions.send_keys(currentDate).send_keys(Keys.RETURN)
 actions.perform()
 
 # delay
+time.sleep(2)
+actions.reset_actions()
+
+# tej34m
+driver.get(tej34mLink)
+
+actions.key_down(Keys.LEFT_ALT).key_down(Keys.LEFT_SHIFT)
+actions.send_keys("i").send_keys("c")
 time.sleep(1)
+actions.send_keys("c")
+actions.key_up(Keys.LEFT_ALT).key_up(Keys.LEFT_SHIFT)
+actions.send_keys(currentDate).send_keys(Keys.RETURN)
+actions.perform()
 
 
+# delay
+time.sleep(2)
 
 # quit browser
 driver.quit()
